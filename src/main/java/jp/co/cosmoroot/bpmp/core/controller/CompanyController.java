3971768e804b9862876595beb963dddb80b34229
tone takafumi <t_tone@cosmoroot.co.jp>
2024-09-13 10:52:21 +0900

    ログ表示機能追加

--- src/main/java/jp/co/cosmoroot/bpmp/core/controller/CompanyController.java
+++ src/main/java/jp/co/cosmoroot/bpmp/core/controller/CompanyController.java
@@ -3,6 +3,8 @@ package jp.co.cosmoroot.bpmp.core.controller;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +21,15 @@ import jp.co.cosmoroot.bpmp.core.service.CompanyService;
 @CrossOrigin(origins = "*")
 public class CompanyController {
 
+    private static final Logger logger = LogManager.getLogger(CompanyController.class);
+
     @GetMapping("/companyList")
     @ResponseBody
     public List<Company> getCompanyList() throws SQLException {
+        logger.info("START");
         CompanyService companyService = new CompanyService();
         List<Company> result = companyService.viewCompanyList();
+        logger.info("END");
         return result;
     }
 
